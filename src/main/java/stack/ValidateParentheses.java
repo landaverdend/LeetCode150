0da81c9c1870bcfd90824887446410aac8fb33c5
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-08-17 23:05:27 -0300

    noice

--- src/main/java/stack/ValidateParentheses.java
+++ src/main/java/stack/ValidateParentheses.java
@@ -1,40 +1,36 @@
 package stack;
 
-import java.util.Set;
+import java.util.Map;
 import java.util.Stack;
 
 public class ValidateParentheses {
 
-    public static boolean isValid(String s) {
+    static final Map<Character, Character> MAP = Map.of(
+            ')','(',
+            '}','{',
+            ']','[');
 
+    public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        Set<Character> openers = Set.of('(', '[', '{');
 
         for (char c : s.toCharArray()) {
-            if (openers.contains(c)) {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) return false;
 
-                char lastPushed = stack.pop();
-                if (!matches(c, lastPushed)) return false;
+            if (!MAP.containsKey(c)) {
+                stack.push(c);
+            }
+            // Character is a closer.
+            else {
+                char expected = MAP.get(c);
+                if (stack.isEmpty() || expected != stack.pop()) return false;
             }
         }
 
+
         return stack.isEmpty();
     }
-
-    // could use a map too...
-    public static boolean matches(char c, char lastPopped) {
-        return (c == ')' && lastPopped == '(')
-                || (c == ']' && lastPopped == '[')
-                || (c == '}' && lastPopped == '{');
-
-    }
-
     public static void main(String[] args) {
         String input = "[";
 
-        System.out.println(String.format("Input String: %s isValid?: ", input, isValid(input)));
+        System.out.println(String.format("Input String: %s isValid?: %s", input, isValid(input)));
     }
 }
