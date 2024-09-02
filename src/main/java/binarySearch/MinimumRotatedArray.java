8ca1c100b30861704b961553ef159ad06a69ab41
Nicodemus  Landaverde <nlandaverde@Nicodemuss-MacBook-Air.local>
2024-09-02 13:40:01 -0300

    minimum sorted array.

---
+++ src/main/java/binarySearch/MinimumRotatedArray.java
@@ -0,0 +1,34 @@
+package binarySearch;
+
+public class MinimumRotatedArray {
+    public static int findMin(int[] nums) {
+
+        int l = 0; int r = nums.length - 1;
+        int min = Integer.MAX_VALUE;
+
+        // Do a check to see if the array is in original sorted order. No work required, just return L
+        if (nums[l] < nums[r]) return nums[l];
+
+
+        while (l <= r) {
+            int m = (r - l) / 2 + l;
+            min = Math.min(nums[m], min);
+
+            if(nums[r] > nums[m]) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return min;
+    }
+
+
+    public static void main(String[] args) {
+        int[] in = new int[] {3,4,5,1,2};
+
+        System.out.printf("Minimum is: %d", findMin(in));
+
+    }
+}
